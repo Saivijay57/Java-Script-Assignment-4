a50e00fc2fcdf2f22bbde975599bde25bf07a742
Saivijay57 <korvisaivijay@gmail.com>
2021-12-13 22:22:25 +0530

    final commit

--- assignments/todo_list/script.js
+++ assignments/todo_list/script.js
@@ -1,23 +1,17 @@
-let inputValue=document.getElementsByTagName('input')[0];
-let Addtobutton=document.getElementsByTagName('button')[0];
-let divtodos=document.getElementsByClassName('todos')[0];
-var x=0;
-Addtobutton.onclick=displayInput;
-function displayInput(){
-    if(inputValue.value == "" || inputValue.value==0){
-        alert("invalid input")
+var input = document.querySelector("input");
+var button = document.querySelector("button")
+var todos = document.querySelector("div.todos")
+let count = 0
+button.addEventListener("click",function(){
+    if (input.value){
+        var newTodo = document.createElement("p")
+        newTodo.innerText = input.value
+        newTodo.setAttribute("key",count++)
+        todos.appendChild(newTodo)
+        newTodo.addEventListener("click",function(e){
+            var key = e.target.getAttribute("key")
+            document.querySelector('p[key="${key}"]').remove();
+        })
+        input.value = "";
     }
-    else {
-        var insidetag=document.createElement('p');
-        var attri=document.createAttribute('key');
-        attri.value=x;
-        insidetag.setAttributeNode(attri);
-        insidetag.innerHTML=inputValue.value;
-        divtodos.appendChild(insidetag);
-        inputValue.value="";
-        x++;
-        insidetag.onclick=function () {
-            divtodos.removeChild(insidetag);
-        }
-    }
-}
+})
